db093a7cbad6971b5a7ed25a6ee13c57fdfd067c
MMyeong <ilikerunning@naver.com>
2023-04-05 02:13:09 +0900

    test: test ci

--- scripts/send_message.py
+++ scripts/send_message.py
@@ -7,14 +7,15 @@ import click, requests
 @click.command(help="CLI for sending message to slack.")
 @click.option("--google_api_key", "-k", type=click.STRING, required=True)
 @click.option("--playlist_id", "-p", type=click.STRING, required=True)
-def main(google_api_key: str, playlist_id:str) -> None:
+@click.option("--webhook_url", "-w", type=click.STRING, required=True)
+def main(google_api_key: str, playlist_id:str, webhook_url:str) -> None:
     youtube_client = youtubeClient(google_api_key)
     union_client = unionClient()
     youtube_url = youtube_client.get_today_vidio(playlist_id)
     body_bible = union_client.fetch_body_bible()
     body_bible_content = union_client.fetch_body_bible_content()
     message = create_message(youtube_url, body_bible, body_bible_content)
-    send_message(message)
+    send_message(webhook_url,message)
 
 
 def create_message(
@@ -39,8 +40,7 @@ def create_message(
     }
 
 
-def send_message(message: str) -> None:
-    url = "https://hooks.slack.com/services/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"
+def send_message(webhook_url:str, message: str) -> None:
     pre_text = message["date"]+ "\n" + message["title"]
     data = {
         "attachments":[
@@ -62,9 +62,9 @@ def send_message(message: str) -> None:
                 }]
             },
         ],
-        "text":message["url"]
+        "text":f"<{message['url']}|YOUTUBE 아까배>"
     }
-    requests.post(url=url, json=data)
+    requests.post(url=webhook_url, json=data)
 
     
 
